1a00141cf13c5752de0bf955163555e8d0616494
zc881213@gmail.com <zc881213@gmail.com@efa8672e-9e8a-11de-a8ff-c955b97a4a10>
2009-10-01 16:41:06 +0000

    update include file

--- classifier/include/init.php
+++ classifier/include/init.php
@@ -1,7 +1,7 @@
 <?php
 	require_once("config.php");
 	require_once("db_base.php");
-	require_once("driver.php");
+	require_once("core_driver.php");
 	global $active_db;
 	build_connection($active_db);	
 ?>
