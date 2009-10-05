cbd01ebd5a55562419215e6dfe812cdb7ad9fd33
zc881213@gmail.com <zc881213@gmail.com@efa8672e-9e8a-11de-a8ff-c955b97a4a10>
2009-10-05 05:24:58 +0000

    FIX BUGS

--- classifier/edit_tag.php
+++ classifier/edit_tag.php
@@ -7,7 +7,7 @@ $login = check_logged_in();
 if (!$login){
 	echo "login in fail";
 } else {
-	$query="select t_name from tag";
+	$query="SELECT t_name FROM tag";
 	$result=db_query($query);
 	$num = count($result);
 
@@ -15,14 +15,15 @@ if (!$login){
 
 	for ($i = 0; $i < $num; $i++){
 		echo '	<div class = "edit_tag">
-	<form name = "admin_input" action = "edit_tag_convert.php" method = "post">
-	<div>$result[$i]</div>
-	<input type = "hidden" name = "t_name_pre" value=".$result[$i]."/>
-	</form>
-	</div>';}
+				<form name = "admin_input" action = "edit_tag_convert.php" method = "post">
+					<div>$result[$i]</div>
+					<input type = "hidden" name = "t_name_pre" value=".$result[$i]."/>
+				</form>
+				</div>';
+	}
 		echo '<div class = "submit">
-	<input type = "submit" value = "Edit" />
-	</div>'
+				<input type = "submit" value = "Edit" />
+			  </div>';
 	 
 }
 ?>
