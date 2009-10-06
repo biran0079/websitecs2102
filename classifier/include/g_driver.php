6d8f7d0e214252a9c80f9a9f1f9d0e29a070fd1f
zc881213@gmail.com <zc881213@gmail.com@efa8672e-9e8a-11de-a8ff-c955b97a4a10>
2009-10-06 07:49:27 +0000

    formatter class

--- classifier/include/g_driver.php
+++ classifier/include/g_driver.php
@@ -33,10 +33,45 @@ function g_check_out(){
 	session_start();
 	session_destroy();
 }
-
+/**
+ * return 
+ * @return unknown_type
+ */
 function g_get_user_role(){
 	return $_SESSION['user']['role'];
 }
 
+function g_get_section(){
+	session_start();
+	if (isset($_SESSION['keyword'])){
+		return $_SESSION['keyword'];
+	}	
+	else
+		return DEFAULT_KEY_WORD;
+}
+
+
+function g_get_entry_title(){
+	return "Recently Added";
+}
+
+/**
+ * list
+ */
+function g_formatter_list_add_recently(){
+	$query = " SELECT * FROM post_node ORDER BY date_add DESC LIMIT 10";
+
+	$result = db_query($query);
+	
+	$html_template = '<a href="#t_1#">#t_2#</a>';
+	$formatter = new Formatter($html_template);
+	
+	while ($row = db_fetch_array($result)){
+		$formatter->addContent('t_1',$row['n_url']);
+		$formatter->addContent('t_2',$row['n_name']);
+		$formatter->flush();
+	}
+	return $formatter->finalize();
+}
 
 ?>
--- classifier/test_db.php
+++ classifier/test_db.php
@@ -1,36 +1,8 @@
 <?php
 // for every single file, include this file
 require_once("./include/init.php");
-
-echo "This test programme will list out all users in your database";
-
-//testing the replace function, it works!
-?>
-
-<!--  	Demo how to add comment in html
-		add new user button
-		-->
-		
-<form name="user_input" action="demo_registration.php" method="get">
-<input type="submit" value="Add more" />
-</form>
-
-<?php 
-$query = "SELECT * FROM user WHERE uid > %d";
-
-// this is the standard way to construct a query, logic is a bit funny
-$result = db_query($query, "0");
-
-// this is the right way to write the select user query
-//$result = db_query("SELECT * FROM user");
-
-// test if you have users in database or not
-if (mysql_num_rows($result) == 0)
-	echo "<h3>oops, you have no user in your database, can u manually add some?</h3>";
-else	
-	while ($row = db_fetch_array($result)){	
-		echo('<div><h3>user name:</h3> '. $row['username']."<br>");
-		echo('<h4>password:</h4> '.$row['password']."<br></div>");
-	}
-		
+//echo str_replace("#t_1#","Peter","Hello #t_1#!");
+echo str_replace('#t_1#','http://google.com','<a href="#t_1#">Google</a>');
+echo g_formatter_list_add_recently();
+	
 ?>
