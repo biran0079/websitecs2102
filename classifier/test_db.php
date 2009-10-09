bfabe6b6de8c296d4ffa730babc4ab52db6df9c8
zc881213@gmail.com <zc881213@gmail.com@efa8672e-9e8a-11de-a8ff-c955b97a4a10>
2009-10-09 04:23:56 +0000

    update

--- classifier/test_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-// for every single file, include this file
-require_once("./include/init.php");
-//echo str_replace("#t_1#","Peter","Hello #t_1#!");
-echo str_replace('#t_1#','http://google.com','<a href="#t_1#">Google</a>');
-echo g_formatter_list_add_recently();
-	
-?>
