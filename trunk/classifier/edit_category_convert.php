6ff1a1240fbb35fa6cee76568e5efa9adacd30f5
zc881213@gmail.com <zc881213@gmail.com@efa8672e-9e8a-11de-a8ff-c955b97a4a10>
2009-10-09 03:59:59 +0000

    update

--- trunk/classifier/edit_category_convert.php
+++ trunk/classifier/edit_category_convert.php
@@ -2,23 +2,22 @@
 require_once("template/header.php");
 require_once("include/init.php");
 
-$c_name_pre = $POST['c_name'];
+$c_name_pre = $_POST['c_name'];
+$cid = $_POST['cid'];
 
 echo '
-	<div class = "form">
-	<form name = "admin_input" action = "midman/checkin_category.php" method = "post">
-	<div>Category</div>
-	<input type="hidden" value="edit" name="op">
-	<table>
-	     <tr>
-								<td>Old Category Name</td> 
-								<td><input type="text" name="c_name_pre", value="'.$c_name_pre.'"></td>
-								</tr>
-		 <tr>
-								<td>New Category Name</td>
-								<td><input type="text" name="c_name"></td>
-								</tr>
-	</table> 
+	<div class = "edit_category">
+		<form name = "admin_input" action = "midman/checkin_category.php" method = "post">
+		<div id="title"> Edit Category</div>
+			<input type="hidden" value="edit" name="op">
+		<div> 
+			<label>Category Name</label>
+			<input type="text" name="c_name" value="'.$c_name_pre.'"/>
+			<input type="hidden" name = "cid" value="'.$cid.'"/>
+		</div> 
+		<div>
+			<span> This operation cannot be undo, the change will affect the post nodes related!</span>
+		</div>
 	<div class = "submit">
 	<input type = "submit" value = "Submit" />
 	</div>
