c1a90472e93bf7a8ec95ec4d5358f9fd54b179a5
zc881213@gmail.com <zc881213@gmail.com@efa8672e-9e8a-11de-a8ff-c955b97a4a10>
2009-10-10 19:12:39 +0000

    dummy category search

--- classifier/include/search_lib.php
+++ classifier/include/search_lib.php
@@ -5,17 +5,25 @@
  * @return unknown_type
  */
 
-function s_search(){
-	$key_words = $_POST['searchtext'];
+function s_search(&$result){
+	$key_words = $_POST['search_text'];
 	
-	$sql_search_base = " SELECT DISTINCT(pn.nid),pn.* FROM post_node AS pn ";
-	$sql_search_base.= " INNER JOIN node_tag AS nt ON (nt.nid = pn.nid)";
-	$sql_search_base.= " INNER JOIN tag AS t ON (t.tid = nt.tid)";
-	$sql_search_base.= " INNER JOIN node_category AS nc ON (nc.nid = pn.nid)";
+	
+	
+	$sql_search_category = " SELECT DISTINCT(pn.nid),pn.* FROM post_node AS pn ";
+	$sql_search_category.= " INNER JOIN node_category AS nc ON (nc.nid = pn.nid)";
+	$sql_search_category.= " INNER JOIN category AS c ON (c.cid = nc.cid)";
+	$sql_search_category.= " WHERE ";
+	// CUSTOMIZED WHERE CONDITION
+	
+	// dummy version
+	$sql_search_category.= " LOWER(c_name) LIKE LOWER('%$key_words%')";
+	$sql_search_tag.= " INNER JOIN node_tag AS nt ON (nt.nid = pn.nid)";
+	$sql_search_tag.= " INNER JOIN tag AS t ON (t.tid = nt.tid)";
+	
 	//$sql_search_base.= " nc"
-	$result = db_query($sql_search_base);
+	$result = db_query($sql_search_category);
 	//die();
-	return $result;
 }
 
 ?>
