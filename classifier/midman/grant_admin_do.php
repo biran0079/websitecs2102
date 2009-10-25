42e17a2a4eb1deb8656222241a2c1e6959908f83
zc881213@gmail.com <zc881213@gmail.com@efa8672e-9e8a-11de-a8ff-c955b97a4a10>
2009-10-25 05:32:20 +0000

    update

--- classifier/midman/grant_admin_do.php
+++ classifier/midman/grant_admin_do.php
@@ -1,5 +1,5 @@
 <?php
-require_once("include/init.php");
+require_once("../include/init.php");
 $name=$_POST["user_name"];
 $query="UPDATE user SET role='1' where username= '%s'";
 db_query($query,$name);
